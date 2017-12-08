f55918ecd8fdfadcaa9cc98240937ed765b6ce0a
Florian Zeidler <zeidler@klimakoenner.de>
2017-12-08 15:37:13 +0100

    Change module name

--- config/initializers/rails/active_record/connection_adapters/quoting.rb
+++ config/initializers/rails/active_record/connection_adapters/quoting.rb
@@ -2,7 +2,7 @@
 # This means that when passing a SimpleDelegator (ie. Compendium::Param) into ActiveRecord::Base.find, it'll
 # crash.
 # Override AR::ConnectionAdapters::Quoting to forward a SimpleDelegator's object to be quoted.
-module ActiveRecord::ConnectionAdapters::QuotingWithSimpleDelegator
+module QuotingWithSimpleDelegator
   def quote(value, column = nil)
     return value.quoted_id if value.respond_to?(:quoted_id)
     value = value.__getobj__ if value.is_a?(SimpleDelegator)
