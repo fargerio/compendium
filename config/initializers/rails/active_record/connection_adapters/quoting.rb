d5dd9aa16654ea0b4df6b9fd65025651821f1fdd
Florian Zeidler <zeidler@klimakoenner.de>
2017-12-08 15:31:31 +0100

    refactor Quoting to use prepend instead of alias_method_chain

--- config/initializers/rails/active_record/connection_adapters/quoting.rb
+++ config/initializers/rails/active_record/connection_adapters/quoting.rb
@@ -2,13 +2,14 @@
 # This means that when passing a SimpleDelegator (ie. Compendium::Param) into ActiveRecord::Base.find, it'll
 # crash.
 # Override AR::ConnectionAdapters::Quoting to forward a SimpleDelegator's object to be quoted.
-
-module ActiveRecord::ConnectionAdapters::Quoting
-  def quote_with_simple_delegator(value, column = nil)
+module ActiveRecord::ConnectionAdapters::QuotingWithSimpleDelegator
+  def quote(value, column = nil)
     return value.quoted_id if value.respond_to?(:quoted_id)
     value = value.__getobj__ if value.is_a?(SimpleDelegator)
-    quote_without_simple_delegator(value, column)
+    super(value, column)
   end
-
-  alias_method_chain :quote, :simple_delegator
 end
+
+ActiveRecord::ConnectionAdapters::Quoting.send(
+  :prepend, QuotingWithSimpleDelegator
+)
